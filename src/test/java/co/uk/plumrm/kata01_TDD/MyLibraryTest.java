554a46853dc64ba3f71ee8560af6a036e175ca28
Mark Plumridge <mark.plumridge@googlemail.com>
2018-12-30 19:48:25 +0000

    Added streams

--- src/test/java/co/uk/plumrm/kata01_TDD/MyLibraryTest.java
+++ src/test/java/co/uk/plumrm/kata01_TDD/MyLibraryTest.java
@@ -19,6 +19,7 @@ public class MyLibraryTest {
         myLibrary = MyLibrary.getINSTANCE();
         myLibrary.add(new Book("Clean Architecture", "Robert C. Martin", 10));
         myLibrary.add(new Book("The Clean Coder", "Robert C. Martin", 10));
+        myLibrary.add(new Book("XML Development with Java 2", "O'Reilly", 6));
 
     }
 
@@ -43,8 +44,22 @@ public class MyLibraryTest {
     @DisplayName("Test Get Stream")
     void testGetStream() {
 
+        myLibrary.add(new Book("Under Construction", "Robert C. Martin", 0));
+
         Stream<Book> stream = myLibrary.getStream();
-        stream.sorted(p -> p.getRating()).forEach(System.out::println);
+        // stream.sorted(p -> p.getRating()).forEach(System.out::println);
+
+        // Print list of Books with a high rating
+        System.out.println("Books with a high rating");
+        stream
+                .filter(book -> book.getRating() > 7)
+                .forEach(book -> System.out.println(book.toString()));
+
+        // Print list of Books written by Robert C. Martin
+        stream = myLibrary.getStream();
+        System.out.println("Book written by Uncle Bob");
+        stream.filter(author -> author.getAuthor().equals("Robert C. Martin"))
+                .forEach(book -> System.out.println(book.toString()));
 
     }
 }
